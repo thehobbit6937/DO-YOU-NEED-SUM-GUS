4ff24f90ccae0654b615e36e1fcf3931157a0f3b
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-22 11:10:42 -0400

    Update LogFlume.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/LogFlume.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/LogFlume.java
@@ -1,4 +1,12 @@
 
-public class LogFlume extends Rides {
-
+public class LogFlume extends Rides 
+{
+  private int peopleinride;
+  
+  public LogFlume()
+  {
+    super();
+    peopleinrides = 2;
+  }
+  public LogFlume
 }
