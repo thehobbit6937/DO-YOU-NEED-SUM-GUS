51ae56664ced6f02070867b515300af419dfba1d
musiclover7315 <musiclover7315@gmail.com>
2017-05-23 10:45:30 -0400

    Lauren's stufffffffffffffff
    Added comments

--- FOOD/src/food/FoodAndDrinks.java
+++ FOOD/src/food/FoodAndDrinks.java
@@ -2,28 +2,36 @@ package food;
 import java.util.Scanner;
 public class FoodAndDrinks extends Shops
 {
+	//declaring cost variable
 	private double cost;
+	//constructor without cost as argument
 	public FoodAndDrinks()
 	{
 		cost=0.0;
 	}
+	//constructor with cost
 	public FoodAndDrinks(double c)
 	{
 		cost=c;
 	}
+	//setter method, setting cost equal to value from argument
 	public void setCost(double c)
 	{
 		cost=c;
 	}
+	//getter method, returning cost
 	public double getCost()
 	{
 		return cost;
 	}
+	//determining the price
 	public double price()
 	{
 		Scanner scan=new Scanner(System.in);
 		System.out.println("How much of this item will you be purchasing? ");
+		//setting variable equal to user input
 		int n=scan.nextInt();
+		//computing cost, closing scanner, and returning price
 		double cost=(double)n*getCost();
 		scan.close();
 		return(cost);
