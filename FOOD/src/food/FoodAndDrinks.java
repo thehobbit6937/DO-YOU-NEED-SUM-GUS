374de36d2c518e9a1a6a18b69d001b06da5d26b6
musiclover7315 <musiclover7315@gmail.com>
2017-05-23 11:05:22 -0400

    Update FoodAndDrinks.java
    Added more methods and changed variable type to int

--- FOOD/src/food/FoodAndDrinks.java
+++ FOOD/src/food/FoodAndDrinks.java
@@ -2,37 +2,79 @@ package food;
 import java.util.Scanner;
 public class FoodAndDrinks extends Shops
 {
-	//declaring cost variable
-	private double cost;
-	//constructor without cost as argument
+	//declaring food/drink variables
+	private int hamburger, hotdog, soda, water;
+	//constructor setting food equal to individual prices
 	public FoodAndDrinks()
 	{
-		cost=0.0;
+		hamburger=5;
+		hotdog=4;
+		water=2;
+		soda=3;
 	}
-	//constructor with cost
-	public FoodAndDrinks(double c)
+	//accessor methods
+	public int getHBPrice()
 	{
-		cost=c;
+		return(hamburger);
 	}
-	//setter method, setting cost equal to value from argument
-	public void setCost(double c)
+	public int getHDPrice()
 	{
-		cost=c;
+		return(hotdog);
 	}
-	//getter method, returning cost
-	public double getCost()
+	public int getWPrice()
 	{
-		return cost;
+		return(water);
 	}
-	//determining the price
-	public double price()
+	public int getSPrice()
+	{
+		return(soda);
+	}
+	//methods to return the price for each item
+	//cost of hamburgers
+	public int HBCost()
 	{
 		Scanner scan=new Scanner(System.in);
-		System.out.println("How much of this item will you be purchasing? ");
+		System.out.println("How many hamburgers are you buying? ");
 		//setting variable equal to user input
-		int n=scan.nextInt();
+		int ha=scan.nextInt();
 		//computing cost, closing scanner, and returning price
-		double cost=(double)n*getCost();
+		int cost=ha*getHBPrice();
+		scan.close();
+		return(cost);
+	}
+	//cost of hot dogs
+	public int HDCost()
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.println("How many hotdogs are you buying? ");
+		//setting variable equal to user input
+		int hd=scan.nextInt();
+		//computing cost, closing scanner, and returning price
+		int cost=hd*getHDPrice();
+		scan.close();
+		return(cost);
+	}
+	//cost of water bottles
+	public int WCost()
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.println("How many water bottles are you buying? ");
+		//setting variable equal to user input
+		int w=scan.nextInt();
+		//computing cost, closing scanner, and returning price
+		int cost=w*getWPrice();
+		scan.close();
+		return(cost);
+	}
+	//cost of soda bottles
+	public int SCost()
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.println("How many soda bottles are you buying? ");
+		//setting variable equal to user input
+		int s=scan.nextInt();
+		//computing cost, closing scanner, and returning price
+		int cost=s*getSPrice();
 		scan.close();
 		return(cost);
 	}
