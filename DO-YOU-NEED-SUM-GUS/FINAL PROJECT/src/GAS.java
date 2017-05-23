5974f201c5c0641cdbd27e972b56a08c3ffebfe0
Darth Gimli <thespeedygenius@gmail.com>
2017-05-23 13:42:51 -0400

    Update GAS.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/GAS.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/GAS.java
@@ -2,38 +2,27 @@
 public class GAS extends Rides{
 	private String weather;
 	private float velocity;
-	private String rname;
 	
 	public GAS() {
 		super();
 		weather = "";
 		velocity = 0;
-		setRname("");
 	}
 	
-	public GAS(String nam, int sala, String we, float v, String r) {
-		super(nam, sala);
+	public GAS(String nam, int sala, String rn, String we, float v) {
+		super(nam, sala, rn);
 		weather = we;
 		velocity = v;
-		setRname(r);
-	}
-	
-	public String getRname() {
-		return rname;
-	}
-
-	public void setRname(String rname) {
-		this.rname = rname;
 	}
 
 	public void isSafe() {
 		if(weather.compareTo("sunny")!=0 || weather.compareTo("drizzle")!=0 
 				|| weather.compareTo("overcast")!=0)
 			System.out.println("The ride is safe to use.");
-		System.out.println("The ride is safe to use.");
+		else
+			System.out.println("The ride is UNsafe to use.");
 	}
 	public void printAttributes() {
 		System.out.println("Weather: " + weather + "  Velocity: " + velocity);
 	}
 }
-
