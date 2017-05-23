74e0dfd3e6ab180c713c89a74416df61e3e85636
Darth Gimli <thespeedygenius@gmail.com>
2017-05-23 11:09:03 -0400

    Update FerrisWheel.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/FerrisWheel.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/FerrisWheel.java
@@ -3,27 +3,37 @@ public class FerrisWheel extends Rides {
 	private int numCars;
 	private int radius;
 	private int rounds;
+	private String rname;
 	
 	public FerrisWheel() {
 		numCars = 0;
 		radius = 0;
 		rounds = 0;
+		setRname("");
 	}
-	public FerrisWheel(int c, int r, int ro) {
+	public FerrisWheel(int c, int r, int ro, String rn) {
 		numCars = c;
 		radius = r;
 		rounds = ro;
+		setRname(rn);
 	}
 	
+	public String getRname() {
+		return rname;
+	}
+	public void setRname(String rname) {
+		this.rname = rname;
+	}
 	public void distance() {
 		double circum = Math.PI * 2 * radius;
 		double dist = circum * rounds;
-		System.out.println("You have travelled" + dist + "meters on the Ferris Wheel.");
-		System.out.println("There are" + numCars + "on the Wheel.");
+		System.out.println("You have travelled  " + dist + "  meters on the Ferris Wheel.");
+		System.out.println("There are  " + numCars + "  on the Wheel.");
 	}
 	
 	public void printAttributes() {
 		System.out.println("Number of Cars: " + numCars + "  Radius: " + radius + 
-				"Number of rounds: " + rounds);
+				"  Number of rounds: " + rounds);
 	}
 }
+
