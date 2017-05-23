d6b7e415bfce9caaeb7039291c8047e5570e122e
santuccim <iman80515@gmail.com>
2017-05-23 10:46:00 -0400

    Update Entry.java

--- Finito/src/Finito/Entry.java
+++ Finito/src/Finito/Entry.java
@@ -1,13 +1,17 @@
 package Finito;
 
 public class Entry {
+	//private data--pop is the current population in the park
 	private int pop;
+	//constructor for the entry class
 	public Entry() {
 		pop=0;
 	}
+	//gettor that returns the current population
 	public int getPop() {
 		return pop;
 	}
+	//settor that sets the population
 	public void setPop(int p) {
 		pop=p;
 	}
