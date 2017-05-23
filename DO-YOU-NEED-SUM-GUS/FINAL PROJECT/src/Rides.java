9d98f6467d816a68e76cd6aff1194efe289b1158
Darth Gimli <thespeedygenius@gmail.com>
2017-05-23 11:10:30 -0400

    Update Rides.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/Rides.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/Rides.java
@@ -1,7 +1,15 @@
 
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.util.ArrayList;
+
 public class Rides implements EmpInfo {
 	private String empName;
 	private double empSalary;
+	ArrayList<Rides> names = new ArrayList<Rides>();
 	
 	public Rides() {
 		empName = "";
@@ -28,4 +36,25 @@ public class Rides implements EmpInfo {
 	public void setName(String n) {
 		empName = n;
 	}
+	
+	
+	public void playSound() {
+    	try{
+            AudioInputStream ais = AudioSystem.getAudioInputStream
+            		(new File("/Users/heliotropesicilia/Desktop/boo.wav"));
+            Clip test = AudioSystem.getClip();  
+
+            test.open(ais);
+            test.start();
+
+            while (!test.isRunning())
+                Thread.sleep(10);
+            while (test.isRunning())
+                Thread.sleep(10);
+
+            test.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+     }
+}
 }
