84a5560296890215198a0e91568bece6af334871
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-23 10:51:37 -0400

    Update WaterSlide.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/WaterSlide.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/WaterSlide.java
@@ -1,4 +1,37 @@
 
-public class WaterSlide extends Rides {
-
+public class WaterSlide extends Rides 
+{
+  private double speed;
+	public WaterSlide()
+	{
+		super();
+		speed = 0;
+	}
+	public WaterSlide(String Nam, int gal, double sp)
+	{
+		super(Nam,gal);
+		speed = sp;
+	}
+	public void setSpeed(double x)
+	{
+		speed = x;
+	}
+	public double getSpeed()
+	{
+		return speed;
+	}
+	public void printSlide()
+	{
+		System.out.println("Name : " + getRideName());
+		System.out.println("Gallons of water per  hour : " + getRideGallonsPerHour() + " G/h");
+		System.out.println("Average Speed of person on slide : " + speed + " m/s");
+	}
+	public void printEmpSlide()
+	{
+		for(Employees d: getEmpInfo())
+		{
+			System.out.println(d.getEmpName());
+			System.out.println(d.getSalary());
+		}
+	}
 }
