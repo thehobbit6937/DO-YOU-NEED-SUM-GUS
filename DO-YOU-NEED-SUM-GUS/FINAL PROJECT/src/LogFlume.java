4adb7e248240aeaef582e97f485c94987cb540ca
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-24 11:04:34 -0400

    Update LogFlume.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/LogFlume.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/LogFlume.java
@@ -2,11 +2,30 @@
 public class LogFlume extends Rides 
 {
   private int peopleinride;
+  private int gallonsperhour;
   
   public LogFlume()
   {
     super();
     peopleinrides = 2;
+    gallonsperhour = 1000000;
   }
-  public LogFlume
+  public LogFlume(String ename, double sal, String name, int urmom)
+  {
+    super(ename, sal, name);
+    gallonsperhour = urmom;
+  }
+  public void increaseWaterFlow(int x)
+  {
+    gallonsperhour+=x;
+  }
+  public void decreaseWaterFLow(int y)
+  {
+    gallonsperhour=-y;
+  }
+  public void printLogFlume()
+  {
+    System.out.println("Name : " + getRname());
+		System.out.println("Gallons per Hour : " + gallonsperhour + " G/h");
+  }
 }
