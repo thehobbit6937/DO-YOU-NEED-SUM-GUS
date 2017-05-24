ca57fc81bd606fcc7cb93b81573fab168f2970e9
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-24 11:13:05 -0400

    Update Hurricane.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/Hurricane.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/Hurricane.java
@@ -1,4 +1,20 @@
 
-public class Hurricane extends Rides {
-
+public class Hurricane extends Rides 
+{
+  private int dankness;//dank = how cool it is out of 10
+  
+  public Hurrican()
+  {
+    super();
+    dankness = 9;
+  }
+  public Hurricane(String enam, double sal, String name, double dank)
+  {
+    super(enam, sal, nam);
+    dankness=dank;
+  }
+  public void dank()
+  {
+    
+  }
 }
