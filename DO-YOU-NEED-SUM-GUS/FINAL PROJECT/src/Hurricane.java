70d977ca35fb302a7063fb841b0c2553c473e3f2
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-25 10:57:33 -0400

    Update Hurricane.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/Hurricane.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/Hurricane.java
@@ -1,20 +1,36 @@
 
 public class Hurricane extends Rides 
 {
-  private int dankness;//dank = how cool it is out of 10
+ private int dankness,int gallonsperhour;//dank = how cool it is out of 10
   
-  public Hurrican()
-  {
-    super();
-    dankness = 9;
-  }
-  public Hurricane(String enam, double sal, String name, double dank)
-  {
-    super(enam, sal, nam);
-    dankness=dank;
-  }
-  public void dank()
-  {
-    
-  }
+ public Hurricane()
+ {
+   super();
+   dankness = 9;// Hurricance is high on the dankometer 
+   gallonsperhous = 1000;
+ }
+  public Hurricane(String enam, double sal, String name, double dank,int gal)
+ {
+   super(enam, sal, nam);
+   dankness=dank;
+   gallonsperhour = gal;//gallons per hour
+ }
+ public changeDank(int r)
+ {
+   dankness =+ r;//changes how awesome the ride is
+ }
+ public void setGallonsperhour(int r)
+ {
+    gallonsperhour = r;
+ }
+ public int getGallonsperhour()
+ {
+    return gallonsperhour;
+ }
+ public void printDank()
+ {
+    System.out.println("Name : " + getRname);
+    System.out.println("Dankness level : " + dankness);
+    System.out.printll("Gallons Per Hour : " + gallonsperhour);
+ }
 }
