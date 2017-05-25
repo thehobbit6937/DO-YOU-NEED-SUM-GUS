895c075c98cb1ba4849e94203a92e23299bf904d
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-25 10:45:35 -0400

    Update WaterPlayground.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/WaterPlayground.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/WaterPlayground.java
@@ -1,4 +1,34 @@
 
-public class WaterPlayground extends Rides {
-
+public class WaterPlayground extends Rides
+{
+	private int maxkids, gallonsperhour;
+	private boolean isbucketfull;//the "bucket" is a giant a pale of water over the whole playground the drops ever so often
+  
+  public WaterPlayground()
+	{
+		super();
+		maxkids = 0;
+		gallonsperhour = 0;
+		isbucketfull = false;// false means that it is not yet full yet
+	}
+	public WaterPlayground(int max, int gal, boolean poo)
+	{
+		super(?);
+		maxkids = max;
+		gallonsperhour = gal;
+		isbucketfull = poo;
+	}
+	public void fillTheBucket()
+	{// will set the isbucketfull boolean to true
+		isbucketfull = true;
+		System.out.println("It's FILLING UP!!!");
+	}
+	public void pourTheBucket()
+	{
+		if(isbucketfull == false)
+			System.out.println("Needs to be filled");
+		else
+			System.out.println("It's COMING DOWN!!!!");
+      isbucketfull =false;//Changes to false so the program will need the fill the bucket again
+	}
 }
