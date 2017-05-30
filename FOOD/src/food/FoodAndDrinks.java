fdd8f31d17112b0e4c99f254784539f37be76e93
Darth Gimli <thespeedygenius@gmail.com>
2017-05-30 08:29:56 -0400

    Update FoodAndDrinks.java

--- FOOD/src/food/FoodAndDrinks.java
+++ FOOD/src/food/FoodAndDrinks.java
@@ -1,9 +1,10 @@
-package food;
 import java.util.Scanner;
-public class FoodAndDrinks extends Shops
+public class FoodAndDrinks
 {
 	//declaring food/drink variables
 	private int hamburger, hotdog, soda, water;
+	private int chamburger, chotdog, csoda, cwater;
+	Scanner scan=new Scanner(System.in);
 	//constructor setting food equal to individual prices
 	public FoodAndDrinks()
 	{
@@ -11,78 +12,94 @@ public class FoodAndDrinks extends Shops
 		hotdog=4;
 		water=2;
 		soda=3;
+		chamburger=0;
+		chotdog=0;
+		cwater=0;
+		csoda=0;
 	}
 	//accessor methods
 	public int getHBPrice()
 	{
-		return(hamburger);
+		return(chamburger);
+	} 
+	public void setCHB(int h) {
+		chamburger = h;
 	}
 	public int getHDPrice()
 	{
-		return(hotdog);
+		return(chotdog);
+	}
+	public void setCHD(int hd) {
+		chotdog = hd;
 	}
 	public int getWPrice()
 	{
-		return(water);
+		return(cwater);
+	}
+	public void setCW(int w) {
+		cwater = w;
 	}
 	public int getSPrice()
 	{
-		return(soda);
+		return(csoda);
+	}
+	public void setCS(int s) {
+		csoda = s;
 	}
 	//methods to return the price for each item
 	//cost of hamburgers
 	public int HBCost()
 	{
-		Scanner scan=new Scanner(System.in);
 		System.out.println("How many hamburgers are you buying? ");
 		//setting variable equal to user input
 		int ha=scan.nextInt();
 		//computing cost, closing scanner, and returning price
-		int cost=ha*getHBPrice();
-		scan.close();
+		int cost=ha*hamburger;
+		setCHD(cost);
 		return(cost);
 	}
 	//cost of hot dogs
 	public int HDCost()
 	{
-		Scanner scan=new Scanner(System.in);
 		System.out.println("How many hotdogs are you buying? ");
 		//setting variable equal to user input
 		int hd=scan.nextInt();
 		//computing cost, closing scanner, and returning price
-		int cost=hd*getHDPrice();
-		scan.close();
+		int cost=hd*hotdog;
+		setCHB(cost);
 		return(cost);
 	}
 	//cost of water bottles
 	public int WCost()
 	{
-		Scanner scan=new Scanner(System.in);
 		System.out.println("How many water bottles are you buying? ");
 		//setting variable equal to user input
 		int w=scan.nextInt();
 		//computing cost, closing scanner, and returning price
-		int cost=w*getWPrice();
-		scan.close();
+		int cost=w*water;
+		setCW(cost);
 		return(cost);
 	}
 	//cost of soda bottles
 	public int SCost()
 	{
-		Scanner scan=new Scanner(System.in);
 		System.out.println("How many soda bottles are you buying? ");
 		//setting variable equal to user input
 		int s=scan.nextInt();
 		//computing cost, closing scanner, and returning price
-		int cost=s*getSPrice();
-		scan.close();
+		int cost=s*soda;
+		setCS(cost);
 		return(cost);
 	}
 	//returning total cost by adding each individual cost
 	public void total()
 	{
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Your total is " + (HBCost()+HDCost()+WCost()+SCost()) + ".");
-		scan.close();
+		System.out.println("Your total is " + (getHBPrice()+getHDPrice()+
+				getWPrice()+getSPrice()) + " dollars.");
+	}
+	
+	public double totalCosts()
+	{
+		return getHBPrice()+getHDPrice()+getWPrice()+getSPrice();
 	}
 }
