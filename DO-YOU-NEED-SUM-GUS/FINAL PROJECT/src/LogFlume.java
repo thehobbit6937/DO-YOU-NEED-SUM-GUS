082b9a13a679bdcdcd46b326ab3478921e08ad8f
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-31 23:10:44 -0400

    Update LogFlume.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/LogFlume.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/LogFlume.java
@@ -1,31 +1,40 @@
-
-public class LogFlume extends Rides 
+public class LogFlume extends Rides //Log Flume is practically a water slide except you are in a log 
 {
-  private int peopleinride;
-  private int gallonsperhour;
+ 	private int peopleinride;// amount of people that can fit in a log
+ 	private int gallonsperhour;
   
-  public LogFlume()
-  {
-    super();
-    peopleinrides = 2;
-    gallonsperhour = 1000000;
-  }
-  public LogFlume(String ename, double sal, String name, int urmom)
-  {
-    super(ename, sal, name);
-    gallonsperhour = urmom;
-  }
-  public void increaseWaterFlow(int x)
-  {
-    gallonsperhour+=x;
-  }
-  public void decreaseWaterFLow(int y)
-  {
-    gallonsperhour=-y;
-  }
-  public void printLogFlume()
-  {
-    System.out.println("Name : " + getRname());
+ 	public LogFlume()
+ 	{
+ 		super();
+ 		peopleinride = 2;
+ 		gallonsperhour = 1000000;
+ 	}
+ 	public LogFlume(String ename, double sal, String name, int pe, int gal)
+ 	{
+ 		super(ename, sal, name);
+ 		peopleinride = pe;
+ 		gallonsperhour = gal;
+ 	}
+ 	public void increaseWaterFlow(int x)//increase water flow
+ 	{
+ 		gallonsperhour= gallonsperhour+x;
+ 	}
+ 	public void decreaseWaterFLow(int y)//decrease water flow
+ 	{
+ 		gallonsperhour= gallonsperhour-y;
+ 	}
+ 	public void expandLog()//increases the seating capacity by 1
+ 	{
+ 		peopleinride= peopleinride + 1;
+ 	}
+ 	public void contractLog()//decreases the seating capacity by 1
+ 	{
+ 		peopleinride= peopleinride - 1;
+ 	}
+ 	public void printLogFlume()
+ 	{
+ 		System.out.println("Name : " + getRname());
+ 		System.out.println("People in the log : " + peopleinride);
 		System.out.println("Gallons per Hour : " + gallonsperhour + " G/h");
-  }
+ 	}
 }
