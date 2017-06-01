af626f18ab23a0fc29c773b98903f11580c854b7
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-31 23:08:31 -0400

    Update WaterPlayground.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/WaterPlayground.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/WaterPlayground.java
@@ -1,52 +1,55 @@
-
 public class WaterPlayground extends Rides
 {
 	private int maxkids, gallonsperhour;//maxkids: the amount of kids on the ride, gallonsperhuor: how many gallons perhour
 	private boolean isbucketfull;//the "bucket" is a giant a pale of water over the whole playground the drops ever so often
-  
-  public WaterPlayground()
+	
+	public WaterPlayground()
 	{
 		super();
 		maxkids = 0;
-		gallonsperhour = 0;
+		gallonsperhour = 0;// how many gallons of water per hour
 		isbucketfull = false;// false means that it is not yet full yet
 	}
-	public WaterPlayground(String em, double sal, String r, int max, int gal, boolean poo)//constructor
+	public WaterPlayground(String em, double sal, String r, int max, int gal, boolean poo)
 	{
 		super(em, sal, r);
 		maxkids = max;
 		gallonsperhour = gal;
 		isbucketfull = poo;
 	}
+	public int getMax()
+	{
+		return maxkids;
+	}
+	public void setAmp(int x)
+	{
+		maxkids = x;
+	}
+	public void setGallonsperhour(int r)
+	{
+		gallonsperhour = r;
+	}
+	public int getGallonsperhour()
+	{
+		return gallonsperhour;
+	}
 	public void fillTheBucket()
 	{// will set the isbucketfull boolean to true
 		isbucketfull = true;
 		System.out.println("It's FILLING UP!!!");
 	}
-	public void pourTheBucket()
+	public void pourTheBucket()//pours the bucket and makes the bucket now empty by setting it to false
 	{
 		if(isbucketfull == false)
 			System.out.println("Needs to be filled");
 		else
 			System.out.println("It's COMING DOWN!!!!");
-      		isbucketfull =false;//Changes to false so the program will need the fill the bucket again
-	}
-	 public void setGallonsperhour(int r)//setter
- 	{
-		gallonsperhour = r;
-	}
- 	public int getGallonsperhour()//getter
-	{
-		return gallonsperhour;
 	}
 	public void printPlay()
 	{
 		System.out.println("Name : " + getRname());
-		System.out.println("Average Speed of person on slide : " + speed + " m/s");
-		if(isbucketfull == false)// this is to tell the reader that if the bucket is full or not
-			System.out.println("The bucket is empty");
-		else
-			System.out.println("The bucket is full");
+		System.out.println("Employee : " + getName());
+		System.out.println("Maximum amount of kids : " + maxkids);
+		System.out.println("Gallons Per Hour : " + gallonsperhour + " G/h");
+	}
 }
-
-
