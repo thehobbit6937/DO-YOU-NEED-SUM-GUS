5137c452851d26c47c409208ad9fc175996fea6d
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-31 23:09:05 -0400

    Update Hurricane.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/Hurricane.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/Hurricane.java
@@ -1,36 +1,36 @@
-
-public class Hurricane extends Rides 
-{
- private int dankness,int gallonsperhour;//dank = how cool it is out of 10
+public class Hurricane extends Rides // a ride that shoots you down than you back and forth
+	{
+	private int dankness, gallonsperhour;//dank = how cool it is out of 10
   
- public Hurricane()
- {
-   super();
-   dankness = 9;// Hurricance is high on the dankometer 
-   gallonsperhour = 1000;
- }
-  public Hurricane(String enam, double sal, String name, double dank,int gal)
- {
-   super(enam, sal, nam);
-   dankness=dank;
-   gallonsperhour = gal;//gallons per hour
- }
- public changeDank(int r)
- {
-   dankness =+ r;//changes how awesome the ride is
- }
- public void setGallonsperhour(int r)
- {
-    gallonsperhour = r;
- }
- public int getGallonsperhour()
- {
-    return gallonsperhour;
- }
- public void printDank()
- {
-    System.out.println("Name : " + getRname());
-    System.out.println("Dankness level : " + dankness);
-    System.out.println("Gallons Per Hour : " + gallonsperhour);
- }
+	public Hurricane()
+	{
+		super();
+		dankness = 9;// Hurricane is high on the dankometer 
+		gallonsperhour = 1000;
+	}
+	public Hurricane(String enam, double sal, String nam, int dank, int gal)
+	{
+		super(enam, sal, nam);
+		dankness=dank;
+		gallonsperhour = gal;//gallons per hour
+	}
+	public void changeDank(int r)
+	{
+		dankness =+ r;//changes how awesome the ride is
+	}
+	public void setGallonsperhour(int r)
+	{
+		gallonsperhour = r;
+	}
+	public int getGallonsperhour()
+	{
+		return gallonsperhour;
+	}
+	public void printDank()
+	{
+		System.out.println("Name : " + getRname());
+		System.out.println("Employee : " + getName());// some have this and some don't this is just to see of the interface works
+		System.out.println("Dankness level : " + dankness);
+		System.out.println("Gallons Per Hour : " + gallonsperhour + " G/h");
+	}
 }
