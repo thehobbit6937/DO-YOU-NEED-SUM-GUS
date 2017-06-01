f3833412612ceef94f0cb269b984e1ec182eb2a6
thehobbit6937 <marcbischoff2000@gmail.com>
2017-05-31 23:07:59 -0400

    Update WaterSlide.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/WaterSlide.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/WaterSlide.java
@@ -1,16 +1,19 @@
 
 public class WaterSlide extends Rides 
 {
-  private double speed;
+	private double speed;
+	private int gallonsperhour;
 	public WaterSlide()
 	{
 		super();
 		speed = 0;
+		gallonsperhour = 300;
 	}
-	public WaterSlide(String enam, double sal, String name, double sp)
+	public WaterSlide(String enam, double sal, String name, double sp, int gal)
 	{
-		super(enam, sal, nam);
+		super(enam, sal, name);
 		speed = sp;
+		gallonsperhour = gal;
 	}
 	public void setSpeed(double x)
 	{
@@ -20,9 +23,18 @@ public class WaterSlide extends Rides
 	{
 		return speed;
 	}
+	public void setGallonsperhour(int r)
+	{
+		gallonsperhour = r;
+	}
+	public int getGallonsperhour()
+	{
+		return gallonsperhour;
+	}
 	public void printSlide()
 	{
 		System.out.println("Name : " + getRname());
 		System.out.println("Average Speed of person on slide : " + speed + " m/s");
+		System.out.println("Gallons Per Hour : " + gallonsperhour + " G/h");
 	}
 }
