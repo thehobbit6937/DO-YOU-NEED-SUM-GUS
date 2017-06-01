efe691235de262972c8e1c5043b7a72a05c41fe5
Darth Gimli <thespeedygenius@gmail.com>
2017-06-01 10:42:04 -0400

    Update AmusePark.java

--- DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/AmusePark.java
+++ DO-YOU-NEED-SUM-GUS/FINAL PROJECT/src/AmusePark.java
@@ -31,6 +31,16 @@ public class AmusePark {
 		listI.add(fwI);
 		Pharoah pI = new Pharoah("Mookalook", 30000, "Pendulum of Death", 30, 50, 5, 1000);
 		listI.add(pI);
+		WaterSlide yes = new WaterSlide("Person", 56000, "Water Slide", 50, 1000);
+		listI.add(yes);
+		WaterPlayground we = new WaterPlayground("Bob", 20000, "Water Playground", 78, 3437, false);
+		listI.add(we);
+		Hurricane knights = new Hurricane("Mikel", 20000, "Hurricane", 9, 300);
+		listI.add(knights);
+		WavePool of = new WavePool("Man", 42000, "Wave Pool", 8, 5.7, 6.9, 15);
+		listI.add(of);
+		LogFlume NE = new LogFlume("Female", 634521, "Log Flume", 2, 234235);
+		listI.add(NE);
 		FoodAndDrinks yum = new FoodAndDrinks();
 		Shops stuff = new Shops();
 		Ticket tI = new Ticket();
@@ -39,6 +49,7 @@ public class AmusePark {
 		Entry entry = new Entry();
 		entry.setPop(50000);
 		
+		
 		System.out.println("Ticket Purchase: ");
 		System.out.println("Fast Pass Price: " + tI.getFPPrice());
 		System.out.println("Regular Ticket Price: " + tI.getTixPrice());
@@ -46,9 +57,22 @@ public class AmusePark {
 		tI.getNumSold();
 		
 		System.out.println(" ");
+		System.out.println("Ride Names: ");
+		for(int i = 0; i<listI.size(); i++) {
+			if(i == (listI.size()-1))      
+				System.out.println(listI.get(i).getRname() + ".");
+			else
+				System.out.print(listI.get(i).getRname() + ", ");
+		}
+		
+		System.out.println(" ");
+		System.out.println("Indoor Ride Information: ");
+		
+		System.out.println(" ");
 		System.out.println("GAS Information: ");
+		
 		System.out.println("Employee Name: " + gasI.getName() + 
-				"  Employee Salary: " + gasI.getSalary());
+				"  Employee Salary: " + comma(gasI.getSalary()));
 		gasI.isSafe();
 		if(pplI.getAge() > gasI.getMinAge() && pplI.getHeight() > gasI.getMinHeight())
 			System.out.println("Person 1 may ride this ride.");
@@ -58,8 +82,9 @@ public class AmusePark {
 		
 		System.out.println(" ");
 		System.out.println("Gravitron Information: ");
+		
 		System.out.println("Employee Name: " + gravI.getName() + 
-				"  Employee Salary: " + gravI.getSalary());
+				"  Employee Salary: " + comma(gravI.getSalary()));
 		gravI.force();
 		gravI.safeGs();
 		if(pplII.getAge() > gravI.getMinAge() && pplII.getHeight() > gravI.getMinHeight())
@@ -71,7 +96,7 @@ public class AmusePark {
 		System.out.println(" ");
 		System.out.println("Haunted House Information: ");
 		System.out.println("Employee Name: " + hhI.getName() + 
-				"  Employee Salary: " + hhI.getSalary());
+				"  Employee Salary: " + comma(hhI.getSalary()));
 		hhI.scary();
 		if(hhI.popup()) {
 			System.out.println("BOO!");
@@ -86,14 +111,14 @@ public class AmusePark {
 		System.out.println(" ");
 		System.out.println("Comet Information: ");
 		System.out.println("Employee Name: " + cometI.getName() + 
-				"  Employee Salary: " + cometI.getSalary());
+				"  Employee Salary: " + comma(cometI.getSalary()));
 		cometI.scareRating();
 		cometI.printAttributes();
 		
 		System.out.println(" ");
 		System.out.println("Ferris Wheel Information: ");
 		System.out.println("Employee Name: " + fwI.getName() + 
-				"  Employee Salary: " + fwI.getSalary());
+				"  Employee Salary: " + comma(fwI.getSalary()));
 		fwI.distance();
 		if(pplII.getAge() > fwI.getMinAge() && pplII.getHeight() > fwI.getMinHeight())
 			System.out.println("Person 2 may ride this ride.");
@@ -102,16 +127,9 @@ public class AmusePark {
 		fwI.printAttributes();
 		
 		System.out.println(" ");
-		System.out.println("Ride Names: ");
-		for(int i = 0; i<listI.size(); i++) {
-			if(i == (listI.size()-1))      
-				System.out.println(listI.get(i).getRname() + ".");
-			else
-				System.out.print(listI.get(i).getRname() + ", ");
-		}
-		
-		System.out.println(" ");
 		System.out.println("Pharoah Information: ");
+		System.out.println("Employee Name: " + pI.getName() + 
+				"  Employee Salary: " + comma(pI.getSalary()));
 		System.out.println("The position you are at is: " + pI.distance() + " m");
 		pI.velocity();
 		pI.acceleration();
@@ -123,6 +141,56 @@ public class AmusePark {
 			System.out.println("Person 1 may ride this ride.");
 		else
 			System.out.println("Person 1 may not ride this ride.");
+		
+		System.out.println(" ");
+		System.out.println("Outdoor Ride Information: ");
+		
+		System.out.println(" ");
+		System.out.println("Water Slide Information: ");
+		
+		System.out.println("Employee Name: " + yes.getName() + 
+				"  Employee Salary: " + comma(yes.getSalary()));
+		yes.printSlide();
+		
+		
+		System.out.println(" ");
+		System.out.println("Water Playground Information: ");
+		
+		System.out.println("Employee Name: " + we.getName() + 
+				"  Employee Salary: " + comma(we.getSalary()));
+		we.fillTheBucket();
+		we.pourTheBucket();
+		System.out.print("\n");
+		we.printPlay();
+		
+		System.out.println(" ");
+		System.out.println("Hurricane Information: ");
+		
+		System.out.println("Employee Name: " + knights.getName() + 
+				"  Employee Salary: " + comma(knights.getSalary()));
+		knights.printDank();
+		knights.changeDank(3);
+		
+		System.out.println(" ");
+		System.out.println("Wave Pool Information: ");
+		
+		System.out.println("Employee Name: " + of.getName() + 
+				"  Employee Salary: " + comma(of.getSalary()));
+		of.decreaseHeight(4);
+		of.increaseHeight(2);
+		of.printWaves();
+	
+		System.out.println(" ");
+		System.out.println("Log Flume Information: ");
+		
+		System.out.println("Employee Name: " + NE.getName() + 
+				"  Employee Salary: " + (NE.getSalary()));
+		NE.contractLog();
+		NE.expandLog();
+		NE.expandLog();
+		NE.decreaseWaterFLow(245);
+		NE.increaseWaterFlow(357);
+		NE.printLogFlume();
 		
 		System.out.println(" ");
 		System.out.println("Food Information: ");
@@ -142,8 +210,10 @@ public class AmusePark {
 		stuff.total(stuff.getCupPrice(), stuff.getHoodPrice(), 
 				stuff.getPicPrice(), stuff.getShirtPrice(), stuff.getSWPPrice());
 		
+		
 		System.out.println(" ");
 		System.out.println("Total Profits: ");
+		
 		double numFP = .2 * entry.getPop();
 		double numReg = entry.getPop() - numFP;
 		double util = 0;
@@ -155,40 +225,6 @@ public class AmusePark {
 		util = .1 * profit;
 		System.out.println("The total profit of the day is " + comma(util));
 		
-		WaterSlide yes = new WaterSlide("Person", 5.60, "Water Slide", 50, 1000);
-		yes.printSlide();
 		
-		System.out.print("\n");
-		
-		WaterPlayground we = new WaterPlayground("Bob", 2, "Water Playground", 78, 3437, false);
-		we.fillTheBucket();
-		we.pourTheBucket();
-		System.out.print("\n");
-		we.printPlay();
-		
-		System.out.print("\n");
-		
-		Hurricane knights = new Hurricane("Mikel", 2, "Hurricane", 9, 300);;
-		knights.printDank();
-		knights.changeDank(3);
-		
-		System.out.print("\n");
-		
-		WavePool of = new WavePool("Man", 4.20, "Wave Pool", 8, 5.7, 6.9, 15);
-		of.decreaseHeight(4);
-		of.increaseHeight(2);
-		of.printWaves();
-		
-		System.out.print("\n");
-		
-		LogFlume NE = new LogFlume("Female", 634521, "Log Flume", 2, 234235);
-		NE.contractLog();
-		NE.expandLog();
-		NE.expandLog();
-		NE.decreaseWaterFLow(245);
-		NE.increaseWaterFlow(357);
-		NE.printLogFlume();
-		System.out.println("Employee  : "  + NE.getName());// testing if interface works
-		System.out.println("Employee's Salary : " + NE.getSalary());
 	}
 }
